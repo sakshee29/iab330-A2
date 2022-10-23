db81db72742b2942ef885d226858837a581b1be1
Sakshee Tosniwal <toshniwalsakshee2002@gmail.com>
2022-10-23 21:34:09 +1000

    (Frontend) RoomDashboard.js page completed

--- client/client-react/src/components/RoomDashboard.js
+++ client/client-react/src/components/RoomDashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import rightArrow from '../images/rightArrow.png';
 import { DeviceFrameset } from 'react-device-frameset';
 
 function RoomDashBoard(){
@@ -8,8 +9,15 @@ function RoomDashBoard(){
                 <div className="timebar">
                     <p>9:41</p>
                 </div>
-                <div className="inner-container">
+                <div className="page-title">
                     <h1>Room Dashboard</h1>
+                </div>
+                <div className="rooms-list">
+                    <p>ICU 1</p>
+                    <p id="room-status">In Use</p>
+                    <a href="/">
+                        <img src={rightArrow} className="right-arrow" alt="right arrow"></img>
+                    </a>
                 </div>
             </div>
         </DeviceFrameset>
