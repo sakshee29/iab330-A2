7f86411493c1c53783b54ea7e366efcdab85afac
Dulce Acevedo <dulcedeyanira.acevedo@connect.qut.edu.au>
2022-10-28 17:57:48 +1000

    Server creates datase if it doesnt exist

--- apiExpress/app.js
+++ apiExpress/app.js
@@ -6,38 +6,43 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 
-sequelize.sync({ force: true }).then(() => {
+app.use(cors());  //Connect/Express middleware
+
+//----------Set up the datbases using sequelize
+
+function setup() {
+  History.sync() // Using 'force: true' for demo purposes. It drops the table users if it already exists and then creates a new one.
+  PeopleCounter.sync() // Using 'force: true' for demo purposes. It drops the table users if it already exists and then creates a new one.
+  .then(function() {
+    PeopleCounter.create({ roomNo: 1, count: 0});
+  });
+  Location.sync()
+}
+
+//Creates the room.db database if it doesnt exist and if exists checks that the database tables match the models.
+sequelize.sync({ alter: true }).then(() => {
   setup();
-  console.log('db is ready')
+  console.log('Database is ready to use')
 });
 
-app.use(cors());
+
+//-------------Routes the clientside can access 
 
 app.get("/history", function(request, response) {
   History.findAll().then(function(history) {
     // finds all entries in the users table
-    response.send(history); // sends users back to the page
+    response.send(history); // sends users back to client
   });
 });
 
 app.get("/counter", function(request, response) {
   PeopleCounter.findAll().then(function(counter) {
     // finds all entries in the users table
-    response.send(counter); // sends users back to the page
+    response.send(counter); // sends users back to the client
   });
 });
 
-
-function setup() {
-  History.sync({ force: true }) // Using 'force: true' for demo purposes. It drops the table users if it already exists and then creates a new one.
-  PeopleCounter.sync({ force: true }) // Using 'force: true' for demo purposes. It drops the table users if it already exists and then creates a new one.
-  .then(function() {
-    PeopleCounter.create({ roomNo: 1, count: 0});
-  });
-  Location.sync({ force: true })
-}
-
-// error handler
+// ------------- Error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
